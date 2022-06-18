5f3bb952c2583becd337a5a642cc5f5a21d3393b
Akshat Saini <akshatsaini1@gmail.com>
2022-06-18 19:25:57 -0400

    Update twt.py

--- twt.py
+++ twt.py
@@ -1,5 +1,6 @@
 #%%
 import tweepy
+import csv
 # %%
 # Twitter Developer keys: 
 consumer_key = '3X2sItg0hqlYgiYRI4YdCMFZP'
@@ -7,9 +8,25 @@ consumer_key_secret = 'dnf20RgVDMmjOSJyQfPFoMO5esjbOBaTIpMR3yA8WMeusDpL5j'
 access_token = '1142488668-YYRzZsk0o5hxodbgyx3bJBNV2PFsEFbPvZnfNtW'
 access_token_secret = 'dSUkjQ2l1pGtuICzYo2KHQ6HPqVxZqIZ5dQdJfNuuvCHC'
 #%%
-auth = tweepy.OAuthhandler(consumer_key, consumer_key_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
 
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+# %%
+search_words = "Dogs" #enter your words -> This can be hastags, @mentions or simple text
+# Below avoids the retweets from the extraction. 
+new_search = search_words + " -filter:retweets"
+# %%
+#csvFile = open('tweets', 'a')
+#csvWriter = csv.writer(csvFile)
+
+fetched_tweets = api.search_tweets(new_search, count = 10)
+   # parsing tweets one by one
+print(fetched_tweets)
+
+
+
+#%%
+
 # %%
